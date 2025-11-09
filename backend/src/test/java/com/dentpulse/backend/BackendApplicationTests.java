b79089c290d0e4d4d433183b2cf371cdd0a844a4
Vishara Srinadi <visharasrinadi@gmail.com>
2025-11-09 22:43:10 +0530

    backend initialized.

---
+++ backend/src/test/java/com/dentpulse/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentpulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
