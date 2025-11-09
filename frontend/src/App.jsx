61a9cbc5eb242e0f0bcfc490b20cb84ba1e949db
Vishara Srinadi <visharasrinadi@gmail.com>
2025-11-09 23:15:55 +0530

    frontend initialized with react.js+vite+tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+function App() {
+  
+  return (
+    <>
+   
+       <p className="bg-red-600">
+        Welcome to DentPulse
+      </p>
+    </>
+  )
+}
+
+export default App
