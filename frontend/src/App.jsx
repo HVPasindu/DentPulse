84ed49dd627c57c6c447e061c0e7c128e762fca3
Navee12345GIT <dissanayakanavee@gmail.com>
2025-12-14 18:38:31 +0530

    Manual Tailwind setup fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,15 @@ import AdminLogin from "./pages/AdminLogin";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#E5F7FA] flex items-center justify-center p-4">
-      <AdminLogin />
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-3xl font-bold text-green-400">
+        Tailwind manual setup works ✅
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
