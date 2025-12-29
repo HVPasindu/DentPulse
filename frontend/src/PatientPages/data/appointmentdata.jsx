4358c13cc86c1e3a281565735b03cb700eca60b5
kalanaashen <northernwolf14742@gmail.com>
2025-12-29 21:22:35 +0530

    add new files

---
+++ frontend/src/PatientPages/data/appointmentdata.jsx
@@ -0,0 +1,44 @@
+export const appointmentdata = [
+
+  {
+    id: "apt_001",
+    patientId: "owner_001",
+    patientName: "John Doe",
+    date: "2024-12-28",
+    time: "10:00 AM",
+    status: "Completed",
+    type: "Checkup",
+    notes: "Regular dental checkup",
+
+  },
+  {
+    id: "apt_002",
+    patientId: "owner_001",
+    patientName: "Janith",
+    date: "2024-12-28",
+    time: "10:00 AM",
+    status: "Pending",
+    type: "Checkup",
+    notes: "Regular dental checkup",
+  },
+  {
+    id: "apt_003",
+    patientId: "owner_001",
+    patientName: "Kasun",
+    date: "2024-12-28",
+    time: "10:00 AM",
+    status: "Completed",
+    type: "Checkup",
+    notes: "Regular dental checkup",
+  },
+  {
+    id: "apt_004",
+    patientId: "owner_001",
+    patientName: "John Doe",
+    date: "2024-12-28",
+    time: "10:00 AM",
+    status: "Confirmed",
+    type: "Checkup",
+    notes: "Regular dental checkup",
+  },
+];
---
+++ frontend/src/PatientPages/data/paitentinputs.jsx
@@ -0,0 +1,28 @@
+export const paitentdata=[
+    {
+        id:1,
+        type:"text",
+        name:"Full Name"
+    },
+    {
+        id:2,
+        type:"tel",
+        name:"Contact Number"
+    },
+    {
+        id:3,
+        type:"email",
+        name:"Email Address"
+    },
+    {
+        id:4,
+        type:"date",
+        name:"Date Of Birth"
+    },
+    {
+        id:5,
+        type:"text",
+        name:"Address"
+    },
+ 
+]
