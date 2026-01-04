2d81b8702ddcf4f22a4e8dde2253fdb10ddee939
kalanaashen <northernwolf14742@gmail.com>
2026-01-04 10:51:29 +0530

    add validation for forms

--- frontend/src/LoginRegister Pages/InputCommonCard.jsx
+++ frontend/src/LoginRegister Pages/InputCommonCard.jsx
@@ -1,17 +1,26 @@
-function InputCommonCard({ name, type, value, label,onChange }) {
+function InputCommonCard({ name, type, value, label,onChange,error }) {
+
+   const isPhone = name === "phone";
+  const isDOB = type === "date" && name === "birthDate";
+
   return (
     <>
       <div className="p-1 flex flex-col gap-3">
         <label className="text-cyan-600 text-sm">{label}</label>
         <input
-          type={type}
-          name={name}
-          value={value}
-          onChange={onChange}
-
-          className="border-2  border-cyan-500 rounded-lg p-1.5 hover:border-gray-500 hover:border-2"
-          placeholder={name}
+       type={isPhone ? "tel" : type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        max={isDOB ? new Date().toISOString().split("T")[0] : undefined}
+        maxLength={isPhone ? 10 : undefined}
+        pattern={isPhone ? "[0-9]{10}" : undefined}
+        placeholder={isPhone ? "07XXXXXXXX" : label}
+        className={`p-3 rounded-xl border outline-none ${
+          error ? "border-red-500" : "border-cyan-300"
+        }`}
         />
+        {error && <p className="text-red-500 text-sm">{error}</p>}
       </div>
     </>
   );
