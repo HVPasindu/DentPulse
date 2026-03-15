6457fcf3ca8ad28a41de8b518a4760315125351a
kalanaashen <northernwolf1472@gmail.com>
2026-03-15 22:57:52 +0530

    fix services cards

--- frontend/src/MainInterface Components/ServicesCard.jsx
+++ frontend/src/MainInterface Components/ServicesCard.jsx
@@ -14,18 +14,18 @@ export function Services(props) {
   };
 
   return (
-    <div className=" flex flex-col min-h-[500px] justify-center items-center p-10 bg-white rounded-2xl py-5 border-green-300 border-2 w-[340px] mx-auto hover:shadow-lg hover:scale-105 transition-transform duration-300">
-      <h1 className="font-bold text-2xl text-green-700 pb-2">{props.title}</h1>
+    <div className=" flex flex-col justify-between h-[420px] p-6 bg-white rounded-2xl border-2 border-green-300 w-full max-w-[280px] mx-auto hover:shadow-lg hover:scale-105 transition-transform duration-300">
+      <h1 className="font-bold text-xl text-green-700 pb-2">{props.title}</h1>
       <div>
         {
           <img
             src={props.img}
             alt={props.title}
-            className="w-64 h-72  mb-4 mx-auto hover:scale-110 duration-400"
+            className="w-50 h-60  mb-4 mx-auto hover:scale-110 duration-400"
           />
         }
       </div>
-      <div className=" items-center justify-center">
+      <div className=" text-center overflow-y-auto max-h-[80px]">
          <p
         className={`text-lg text-black text-center transition-all duration-300 ${
           expanded ? "" : "line-clamp-1"
