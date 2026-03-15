2a688adfa8f27b8f7f71a8e29a999159ba63975f
sahanBasnayake <sahanbasnayake888@gmail.com>
2026-03-16 02:13:25 +0530

    change login URL

--- frontend/src/LoginRegister Pages/MainLogin.jsx
+++ frontend/src/LoginRegister Pages/MainLogin.jsx
@@ -45,7 +45,7 @@ const MainLogin = () => {
 
   try {
     const response = await axios.post(
-      `${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`,
+      "https://api.dentpulseclinic.com/api/v1/auth/login",
       formData,
       { headers: { "Content-Type": "application/json" } }
     );
